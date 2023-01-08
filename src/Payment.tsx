01136554f157a6301984b5c0d9f455d0f6dfada2
Juntao Qiu <juntao.qiu@gmail.com>
2023-01-08 19:53:58 +1100

    extracted PaymentMethods component

--- src/Payment.tsx
+++ src/Payment.tsx
@@ -43,25 +43,33 @@ const usePaymentMethods = () => {
   };
 };
 
+const PaymentMethods = ({
+  paymentMethods,
+}: {
+  paymentMethods: LocalPaymentMethod[];
+}) => (
+  <>
+    {paymentMethods.map((method) => (
+      <label key={method.provider}>
+        <input
+          type="radio"
+          name="payment"
+          value={method.provider}
+          defaultChecked={method.provider === "cash"}
+        />
+        <span>{method.label}</span>
+      </label>
+    ))}
+  </>
+);
+
 export const Payment = ({ amount }: { amount: number }) => {
   const { paymentMethods } = usePaymentMethods();
 
   return (
     <div>
       <h3>Payment</h3>
-      <div>
-        {paymentMethods.map((method) => (
-          <label key={method.provider}>
-            <input
-              type="radio"
-              name="payment"
-              value={method.provider}
-              defaultChecked={method.provider === "cash"}
-            />
-            <span>{method.label}</span>
-          </label>
-        ))}
-      </div>
+      <PaymentMethods paymentMethods={paymentMethods} />
       <button>${amount}</button>
     </div>
   );
